ab0ef2a654774aa8da50f04321ab4a6653be0754
Melody Wen <40778015+melo2902@users.noreply.github.com>
2021-07-06 16:06:23 -0400

    Implemented Log-in and Sign-up
    User is able to log in and sign up for an account on the log in page

---
+++ instagram-mock/View Controller/LoginViewController.h
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.h
+//  instagram-mock
+//
+//  Created by mwen on 7/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
