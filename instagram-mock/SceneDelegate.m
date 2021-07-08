c8b22a026d628dc1b4796679c4a526035ee3eb94
Melody Wen <40778015+melo2902@users.noreply.github.com>
2021-07-08 15:21:11 -0400

    Fix persist login error

--- instagram-mock/SceneDelegate.m
+++ instagram-mock/SceneDelegate.m
@@ -32,7 +32,7 @@
     if (PFUser.currentUser) {
         UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
         
-        self.window.rootViewController = [storyboard instantiateViewControllerWithIdentifier:@"HomeFeedViewController"];
+        self.window.rootViewController = [storyboard instantiateViewControllerWithIdentifier:@"UITabBarController"];
     }
 }
 
